9e45c002731c33d98443715161e165a45da8cf93
Tomé Simões <tomefsimoes@gmail.com>
2024-12-17 14:06:49 +0000

    Serve.js file updated

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/", (message, response) => {
+  response.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
