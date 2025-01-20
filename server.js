4d1b9f542f7b19b4d271e80caa83c73c50027ba6
Tomé Simões <tomefsimoes@gmail.com>
2025-01-20 16:51:52 +0000

    Port 3000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 import { MongoClient } from "mongodb";
 
 const app = express();
-const PORT = 3002;
+const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
